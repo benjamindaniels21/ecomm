d3c2c6264432367be7cababa35899ad0bcb5edb9
Ben Daniels <benjamindaniels21@gmail.com>
2023-02-23 19:12:43 -0500

    sending HTML

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hi there!");
+  res.send(`
+    <div>
+      <form>
+        <input placeholder="email" />
+        <input placeholder="password" />
+        <input placeholder="password confirmation" />
+      </form>
+    </div>
+    `);
 });
 
 app.listen(3000, () => {
