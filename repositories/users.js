73529b9e30b45abc618e0b6aeb694f6ca8e7ea73
Ben Daniels <benjamindaniels21@gmail.com>
2023-02-28 18:36:20 -0500

    check if file exists

--- repositories/users.js
+++ repositories/users.js
@@ -1,8 +1,14 @@
+const fs = require("fs");
+
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
       throw new Error("Creating a repository requires a filename.");
     }
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {}
   }
 }
 
