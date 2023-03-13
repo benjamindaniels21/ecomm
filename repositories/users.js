6ddfaac5e3987e516930387af4ce355a3aa20b7b
Ben Daniels <benjamindaniels21@gmail.com>
2023-03-13 11:18:02 -0400

    hashing and salting password

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
   constructor(filename) {
@@ -24,10 +27,17 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    //attrs is an object with an email and password {email: '', password: ''}
     attrs.id = this.randomId();
-    //{email: 'email@email.com', password: 'password'}
+
+    const salt = crypto.randomBytes(8).toString("hex");
+    const buf = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
+    records.push({
+      ...attrs, //copy all properties off attrs
+      password: `${buf.toString("hex")}.${salt}`, //replace password with this
+    });
     //write updates records array back to users.json (this.filename)
     await this.writeAll(records);
 
