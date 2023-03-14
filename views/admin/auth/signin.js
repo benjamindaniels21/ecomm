b2d1378237d8f900b353cf3d42885504d4b5bb6e
Ben Daniels <benjamindaniels21@gmail.com>
2023-03-14 19:14:46 -0400

    working with html

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,11 @@
 module.exports = () => {
   return `
-     <div>
-      <form method="POST">
+    <div>
+    <form method="POST">
         <input name="email" placeholder="email" />
         <input name="password" placeholder="password" />
         <button>Sign In</button>
-      </form>
+    </form>
     </div>
     `;
 };
