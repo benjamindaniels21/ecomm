4212f9b243dd7fd142182d5646b57e6000117ad7
Ben Daniels <benjamindaniels21@gmail.com>
2023-03-15 08:20:08 -0400

    sanitization

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -12,7 +12,11 @@ router.get("/signup", (req, res) => {
 
 router.post(
   "/signup",
-  [check("email").isEmail(), check("password"), check("passwordConfirmation")],
+  [
+    check("email").trim().normalizeEmail().isEmail(),
+    check("password").trim().isLength({ min: 4, max: 20 }),
+    check("passwordConfirmation").trim().isLength({ min: 4, max: 20 }),
+  ],
   async (req, res) => {
     const { email, password, passwordConfirmation } = req.body;
 
