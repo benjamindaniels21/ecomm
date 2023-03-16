cfc42ea30199e8f0aabaa2ab1c3b5ffde92a1168
Ben Daniels <benjamindaniels21@gmail.com>
2023-03-16 07:02:09 -0400

    show errors in html of signin

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -14,7 +14,9 @@ module.exports = ({ errors }) => {
         <div>
         <form method="POST">
             <input name="email" placeholder="email" />
+            ${getError(errors, "email")}
             <input name="password" placeholder="password" />
+            ${getError(errors, "password")}
             <button>Sign In</button>
         </form>
         </div>
