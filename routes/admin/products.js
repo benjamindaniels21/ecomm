f62b6623aa81d8915c760dc5d502d4a620476d24
Ben Daniels <benjamindaniels21@gmail.com>
2023-03-17 07:40:27 -0400

    add products.js

---
+++ routes/admin/products.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/admin/products", (req, rest) => {});
+
+router.get("/admin/products/new", (req, res) => {});
+
+module.exports = router;
