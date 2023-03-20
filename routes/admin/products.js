1652dc1964a065e13778d79c7a5517d7ea3d0a0b
Ben Daniels <benjamindaniels21@gmail.com>
2023-03-19 20:18:45 -0400

    logging out errors

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -2,6 +2,7 @@ const express = require("express");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
+const { validationResult } = require("express-validator");
 
 const router = express.Router();
 
@@ -12,7 +13,8 @@ router.get("/admin/products/new", (req, res) => {
 });
 
 router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  console.log();
+  const errors = validationResult(req);
+  console.log(errors);
 
   res.send("submitted");
 });
