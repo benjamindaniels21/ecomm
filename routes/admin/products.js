95f59af7923c2ed5396f2b1010680f2fd1fe3a85
Ben Daniels <benjamindaniels21@gmail.com>
2023-03-19 20:16:54 -0400

    checking when user submits product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
+const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
 
@@ -8,6 +9,12 @@ router.get("/admin/products", (req, rest) => {});
 
 router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
+});
+
+router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
+  console.log();
+
+  res.send("submitted");
 });
 
 module.exports = router;
