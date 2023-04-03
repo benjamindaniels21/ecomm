15d71299bb756b2d0807bd8e4024c047e5980397
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-02 21:40:01 -0400

    commenting code

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -1,6 +1,7 @@
 const { validationResult } = require("express-validator");
 
 module.exports = {
+  //handle errors in various areas of the app
   handleErrors(templateFunc) {
     return (req, res, next) => {
       const errors = validationResult(req);
@@ -11,6 +12,7 @@ module.exports = {
       next();
     };
   },
+  //make sure user has been authenticated
   requireAuth(req, res, next) {
     if (!req.session.userId) {
       return res.redirect("/signin");
