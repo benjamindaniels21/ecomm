dbb3301c9c18003beb697b4fb5f6e3610a3f4b0d
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-05 13:33:00 -0400

    testing delete product button

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -15,7 +15,7 @@ module.exports = ({ products }) => {
           </a>
         </td>
         <td>
-            <form method="POST" action="admin/products/${product.id}/delete">
+            <form method="POST" action="/admin/products/${product.id}/delete">
                 <button class="button is-danger">Delete</button>
             </form>
         </td>
