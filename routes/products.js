c7a80534a73406738c9ef2af1acba703cc562169
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-12 08:21:24 -0400

    render products to root route

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const productsRepo = require("../repositories/products");
+const productsIndexTemplate = require("../views/products/index");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   const products = await productsRepo.getAll(); //grab all products
-  res.send("Products!!!");
+  res.send(productsIndexTemplate({ products }));
 });
 
 module.exports = router;
