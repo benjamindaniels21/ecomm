9cfa23349db01f12e09940325f1b0e95e964f9ea
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-13 14:11:10 -0400

    adding items to cart and update repo

--- routes/carts.js
+++ routes/carts.js
@@ -1,18 +1,41 @@
 const express = require("express");
-const cartsRepo = require("../carts");
+const cartsRepo = require("../repositories/carts");
 
 const router = express.Router();
 
 //Receive post request to add item to cart
 router.post("/cart/products", async (req, res) => {
   //figure out the cart
+  let cart;
   if (!req.session.cartId) {
     //we don't have a cart, we need to make one
     //store the cart id on the req.session.cartId
-    const cart = await cartsRepo.create({ items: [] });
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
   } else {
     //we have a cart, let's get it from the repo
+    cart = await cartsRepo.getOne(req.session.cartId);
   }
+
+  //Either increment quantity for existing product
+  //OR add new product to items array
+
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+
+  if (existingItem) {
+    //increment quantity
+    existingItem.quantity++;
+  } else {
+    //add to array
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
+
+  res.send("Product Added to Cart");
 });
 
 //receive post request to delete item from cart
