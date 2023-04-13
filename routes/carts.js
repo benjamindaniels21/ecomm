19a3b35b2b5fcf477fad9d740d1c8144565b2f6e
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-13 13:09:14 -0400

    commenting cart

--- routes/carts.js
+++ routes/carts.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+//Receive post request to add item to cart
+
+//receive GET request to show all items in cart
+
+//receive post request to delete item from cart
+
 module.exports = router;
