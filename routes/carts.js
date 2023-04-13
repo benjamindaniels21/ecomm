722faa2151029b6dd7d836a6c62c695516ee2287
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-13 13:35:48 -0400

    connected add button route

--- routes/carts.js
+++ routes/carts.js
@@ -3,7 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 //Receive post request to add item to cart
+router.post("/cart/products", (req, res) => {
+  console.log(req.body.productId);
 
+  res.send("Product added to cart!");
+});
 //receive GET request to show all items in cart
 
 //receive post request to delete item from cart
