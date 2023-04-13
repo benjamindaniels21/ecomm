b86772817361492eeccbe70e0ec437c0b8cfa040
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-13 13:47:22 -0400

    create cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const cartsRepo = require("../carts");
 
 const router = express.Router();
 
 //Receive post request to add item to cart
-router.post("/cart/products", (req, res) => {
-  console.log(req.body.productId);
-
-  res.send("Product added to cart!");
+router.post("/cart/products", async (req, res) => {
+  //figure out the cart
+  if (!req.session.cartId) {
+    //we don't have a cart, we need to make one
+    //store the cart id on the req.session.cartId
+    const cart = await cartsRepo.create({ items: [] });
+  } else {
+    //we have a cart, let's get it from the repo
+  }
 });
-//receive GET request to show all items in cart
 
 //receive post request to delete item from cart
 
