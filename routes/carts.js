280c99031ba71e464ac5890e4c8d644918a6f479
Ben Daniels <benjamindaniels21@gmail.com>
2023-04-13 19:02:41 -0400

    working on the cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const { it } = require("node:test");
 
 const router = express.Router();
 
@@ -36,6 +38,23 @@ router.post("/cart/products", async (req, res) => {
   });
 
   res.send("Product Added to Cart");
+});
+
+//Receive Get request to show all items in cart
+
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
 });
 
 //receive post request to delete item from cart
